f6c2766406a3957ed650ef7e78ec5abf8a5b3fa0
Bruno Rogério <b.rogerio2007@gmail.com>
2021-11-21 18:58:51 -0400

    Exercicios praticando...

--- src/chuteNumero/ExercicioChuteNumero.java
+++ src/chuteNumero/ExercicioChuteNumero.java
@@ -21,10 +21,17 @@ public class ExercicioChuteNumero {
 
             if (chute >= 0 && chute <= 10) {
 
-                while (chute != numeroAlvo) {
-                    System.out.println("Não é o número " + chute + " Tente outro número: ");
-                    chute = entrada.nextInt();
-                }
+                    while (chute != numeroAlvo) {
+
+                        if (chute > numeroAlvo) {
+                            System.out.println("Você chutou acima do alvo");
+                        } else if (chute < numeroAlvo){
+                            System.out.println("Você chutou abaixo do alvo");
+                        }
+
+                        System.out.println("Não é o número " + chute + ", Tente outro número: ");
+                        chute = entrada.nextInt();
+                    }
                 System.out.println("Você acertou! era o número: " + numeroAlvo);
             } else {
                 System.out.println("Você escolheu um número um número fora da faixa 0 a 10");
