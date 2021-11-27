c0aea614a4482cc1411c5ec3234603ee804312c4
Bruno Rogério <b.rogerio2007@gmail.com>
2021-11-27 14:07:52 -0400

    Exercicios praticando...

--- src/exercicios/FatorialNumero.java
+++ src/exercicios/FatorialNumero.java
@@ -1,14 +1,27 @@
 package exercicios;
 
+import javax.swing.*;
 import java.util.Scanner;
+
+import static javax.swing.JOptionPane.showMessageDialog;
 
 public class FatorialNumero {
 
     public static void main(String[] args) {
 
-        Scanner entrada = new Scanner(System.in);
+        int numero, fatorial = 1;
 
-
+        numero = Integer.parseInt(JOptionPane.showInputDialog("Informe um número? "));
+        if (numero < 0) {
+            JOptionPane.showMessageDialog(null, "Não tem fatorial de número negativo");
+            System.out.println("Não tem fatorial de número negativo! ");
+        } else {
+            while (numero > 1) {
+                fatorial *= numero;
+                numero--;
+            }
+            JOptionPane.showMessageDialog(null, "Respota: " + fatorial);
+        }
 
 
     }
