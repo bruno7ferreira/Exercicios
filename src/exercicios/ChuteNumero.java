dea06031b713e57461f90258aa46201c31728b09
Bruno Rogério <b.rogerio2007@gmail.com>
2021-11-28 20:41:44 -0400

    Exercicios praticando...

--- src/exercicios/ChuteNumero.java
+++ src/exercicios/ChuteNumero.java
@@ -12,25 +12,34 @@ public class ChuteNumero {
 
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Informe um número de 1 a 10: ");
-        Integer palpite = entrada.nextInt();
+        Integer palpite;
 
-        if (palpite < 1 || palpite > 10) {
-            if (palpite < 1) {
-                System.out.println("ERRO! Você informou um valor abaixo de 1");
-            } else {
-                System.out.println("ERRO! Você informou um valor acima de 10");
-            }
-        } else {
-            if (palpite == numero) {
-                System.out.println("Parabéns! Você acertou era o número: " + numero);
-            } else {
-                if (palpite > numero) {
-                    System.out.println("errou kkkk, é um número mais baixo!");
+        do {
+            System.out.println("Informe um número de 1 a 10: ");
+            palpite = entrada.nextInt();
+        } while (palpite < 1 || palpite > 10);
+
+        //laço de 1 a 10 ok
+
+        while (palpite != numero){
+            if (palpite < 1 || palpite > 10) {
+                if (palpite < 1) {
+                    System.out.println("ERRO! Você informou um valor abaixo de 1");
                 } else {
-                    System.out.println("errou kkkk, é um número mais alto");
+                    System.out.println("ERRO! Você informou um valor acima de 10");
+                }
+            } else {
+                if (palpite == numero) {
+                    System.out.println("Parabéns! Você acertou era o número: " + numero);
+                } else {
+                    if (palpite > numero) {
+                        System.out.println("errou kkkk, é um número mais baixo!");
+                    } else {
+                        System.out.println("errou kkkk, é um número mais alto");
+                    }
                 }
             }
+
         }
 
 
