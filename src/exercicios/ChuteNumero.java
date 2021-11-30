d1e97e29cbfd982d59979bacb082042da5ea5f5b
Bruno Rogério <b.rogerio2007@gmail.com>
2021-11-29 21:53:45 -0400

    Exercicios praticando...

--- src/exercicios/ChuteNumero.java
+++ src/exercicios/ChuteNumero.java
@@ -21,26 +21,16 @@ public class ChuteNumero {
 
         //laço de 1 a 10 ok
 
-        while (palpite != numero) {
-
-
-            if (palpite < 1) {
-                System.out.println("ERRO! Você informou um valor abaixo de 1");
-            } else {
-                System.out.println("ERRO! Você informou um valor acima de 10");
+        if(palpite!=numero){
+            if (palpite>numero){
+                System.out.println("Você chutou um número acima...");
+                System.out.println("Tente novamente, informe um número de 1 a 10: ");
+                palpite = entrada.nextInt();
+            }else {
+                System.out.println("Você chutou um número abaixo...");
+                System.out.println("Tente novamente, informe um número de 1 a 10: ");
+                palpite = entrada.nextInt();
             }
-
-            if (palpite == numero) {
-                System.out.println("Parabéns! Você acertou era o número: " + numero);
-            } else {
-                if (palpite > numero) {
-                    System.out.println("errou kkkk, é um número mais baixo!");
-                } else {
-                    System.out.println("errou kkkk, é um número mais alto");
-                }
-            }
-
-
         }
         System.out.println("Parabéns você acertou! '" + numero + "'");
 
