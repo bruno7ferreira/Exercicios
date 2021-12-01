946e6ee8b38c2763dc4a8467b5bca9bfbd00b658
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-01 19:54:36 -0400

    Exercicios praticando...

---
+++ src/ExerciciosOptionPane/BoasVindaOpitionPane.java
@@ -0,0 +1,15 @@
+package ExerciciosOptionPane;
+
+import javax.swing.*;
+
+public class BoasVindaOpitionPane {
+
+    public static void main(String[] args) {
+
+        String nome = JOptionPane.showInputDialog("Informe o seu nome: ");
+
+        JOptionPane.showMessageDialog(null, " Seja bem-vindo! " + nome);
+
+    }
+
+}
