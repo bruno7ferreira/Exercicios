61098ef2dae43c4efb7602c4d07c565b4d9e430a
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-01 00:14:35 -0400

    Exercicios praticando...

--- src/ExerciciosOptionPane/ChuteNumeroOptionPane.java
+++ src/ExerciciosOptionPane/ChuteNumeroOptionPane.java
@@ -3,7 +3,6 @@ package ExerciciosOptionPane;
 import javax.swing.*;
 import java.util.Random;
 import java.util.Scanner;
-
 import static java.lang.Integer.parseInt;
 
 
@@ -20,6 +19,7 @@ public class ChuteNumeroOptionPane {
         String palpiteTemp = JOptionPane.showInputDialog("Informe um número de 1 a 10: ");
         Integer palpite = parseInt(palpiteTemp);
 
+        //Repete enquanto o usuário não digitar um número entre 1 e 10
         do {
             palpiteTemp = JOptionPane.showInputDialog("Informe um número de 1 a 10: ");
             palpite = parseInt(palpiteTemp);
@@ -28,7 +28,7 @@ public class ChuteNumeroOptionPane {
 
 
 
-        //laço de 1 a 10 ok
+
 
 
     }
