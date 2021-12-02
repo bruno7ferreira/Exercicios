30760becaf15b610aae017133bc704ae4fd9afa7
Bruno Rogério <b.rogerio2007@gmail.com>
2021-12-01 20:32:56 -0400

    Exercicios praticando...

--- src/exercicios/QualMaiorNumero.java
+++ src/exercicios/QualMaiorNumero.java
@@ -3,25 +3,26 @@ package exercicios;
 import javax.swing.*;
 import java.lang.*;
 
+import static java.lang.Integer.parseInt;
+
 public class QualMaiorNumero {
 
     public static void main(String[] args) {
 
         String tempNum1 = JOptionPane.showInputDialog("Informe o 1° número: ");
-        int num1 = parseInt();
+        int num1 = parseInt(tempNum1);
 
-        System.out.println("Informe o 2° número: ");
-        int num2 = entrada.nextInt();
+        String tempNum2 = JOptionPane.showInputDialog("Informe o 2° número: ");
+        int num2 = parseInt(tempNum2);
 
         if (num1 > num2) {
-            System.out.println("Número " + num1 + " é maior!");
+            JOptionPane.showMessageDialog(null, "Número " + num1 + " é maior!");
         } else {
             if (num2 > num1) {
-                System.out.println("Número " + num2 + " é maior!");
+                JOptionPane.showMessageDialog(null, "Número " + num2 + " é maior!");
             } else {
-                System.out.println("Os números " + num1 + " e " + num2 + " são iguais!");
+                JOptionPane.showMessageDialog(null, "Os números " + num1 + " e " + num2 + " são iguais!");
             }
         }
     }
-
 }
